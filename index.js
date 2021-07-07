791080acd8ff6cf0f667b8b77a81f9a8e9b33578
Le Van Hop <hop.le@axonactive.com>
2021-07-07 16:54:26 +0700

    Update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.post("/events", (req, res) => {
-  const { event } = req.body;
+  const event = req.body;
 
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
